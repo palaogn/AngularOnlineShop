07eee29b3a0e66390da0179d8eb4c605164289b7
dadi sigursveinn Hardarson <dadi.sigursveinn@gmail.com>
2017-03-14 22:40:43 +0000

    all testss in app.component succeed

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -34,18 +34,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  //Fails
-  /*it(`should have as title 'app works!'`, async(() => {
+  //Succeeds
+  it(`should have as title 'Litla hryllingsbúðin'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Litla hryllingsbúðin');
-  }));*/
+  }));
 
-  //Fails
-  /*it('should render title in a h1 tag', async(() => {
+  //Succeeds
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Litla hryllingsbúðin');
-  }));*/
+  }));
 });
